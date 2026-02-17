a88016c89a37090beb476a3332163a138f22f519
Bruno Melo <srbrunomelo@gmail.com>
2026-02-17 07:42:25 -0300

    add new songs

--- src/shared/lib/data.ts
+++ src/shared/lib/data.ts
@@ -14,11 +14,28 @@ export const songs: Song[] = [
     duration: 234, 
     artist: "Bruno Melo", 
     coverUrl: '/images/cover/funk.png',
-    audioUrl: '/songs/feito-louca.mp3',
+    audioUrl: '/songs-web/feito-louca.mp3',
     description: "",
     lyrics: `
       Feito Louca, vem beijando a minha boca
-      vem botando mo pressão neste negão 
+      vem botando mo pressão neste negão
+
+      num pique quente essa mina é envolvente
+      vou meter logo pião nesse mundão
+
+      dentro do barraco o bicho pega
+      ela vem sem frescura
+      nunca duvide do poder de uma mulher
+
+      ela vai por cima dominando
+      é uma loucura
+      to viciado e sei bem o que ela quer
+
+      feito louca, vem beijando minha boca
+      feito louca
+      me leva pra onde ce quiser
+      feito louca, vem beijando minha boca
+      feito louca 
       `
   },
   {
@@ -51,7 +68,112 @@ export const songs: Song[] = [
       Livre em ti
       `
   },
+  {
+    id: "3",
+    title: "Coincidência",
+    category: "Sertanejo",
+    categorySlug: slugify('Sertanejo'),  
+    slug: slugify('Coincidência'),
+    duration: 205, 
+    artist: "Bruno Melo", 
+    coverUrl: '/images/cover/sertanejo.png',
+    audioUrl: '/songs-web/coincidencia.mp3',
+    description: "",
+    lyrics: ` 
+        Não quero acreditar que foi coincidência
+        Você é meu destino,  apesar  dessa ausencia
+        é minha estrela guia é minha paixão
 
+        Não é  medo de te perder pro seu passado
+        É o medo de não ter você aqui do lado
+        nem sei como seria essa solidão
+        
+        Foi amooor, 
+        agora estou sofrendo
+        hoje Eu vivo bebendoo
+        pensando nelaa.
+
+        Oi amor,  eu vou gritar bem alto
+        eu so quero que entendar
+
+        não foi coincidencia
+      `
+  },
+  {
+    id: "4",
+    title: "Memorias",
+    category: "Sertanejo",
+    categorySlug: slugify('Sertanejo'),  
+    slug: slugify('Memorias'),
+    duration: 205, 
+    artist: "Bruno Melo", 
+    coverUrl: '/images/cover/sertanejo.png',
+    audioUrl: '/songs-web/memorias.mp3',
+    description: "",
+    lyrics: `
+      Você anda por aí brincando de fazer amor
+      Mas eu sei em mim foi que você pensou
+      Quando ele não fez o que você queria
+      Sempre fui eu
+      Sei todos mistérios desse corpo seu
+      Tudo era perfeito só você e eu
+      Mas acabou
+      Só resta dor
+
+      Eu sei, sei que ainda lembra dos versos de amor
+      Sei que ainda o sonho não se acabou
+      Sei que quando você vai dormir me chama
+
+      Não tem como apagar o que a gente viveu
+      Era tão perfeito só você e eu
+      Ficou gravado na memória
+      Nem tentando eu consigo te esquecer
+      Você se faz presente em cada amanhecer
+      Em cada verso
+      Não tem como apagar este amor
+      `
+  },
+  {
+    id: "5",
+    title: "Eu Te Quero Além de Tudo",
+    category: "Gospel",
+    categorySlug: slugify('Gospel'),  
+    slug: slugify('Eu Te Quero Além de Tudo'),
+    duration: 205, 
+    artist: "Bruno Melo", 
+    coverUrl: '/images/cover/gospel.png',
+    audioUrl: '/songs-web/eu-te-quero-alem-de-tudo.mp3',
+    description: "",
+    lyrics: ` 
+      Eu te quero mais que tudo
+      Eu te quero além de tudo
+      
+      Toda terra  e mar
+      mais que o respirar
+      Eu te quero além de tudo
+      Eu te quero mais que tudo
+
+      Toda terra  e mar
+      mais que o respirar
+      Eu te quero além de tudo
+      Eu te quero mais que tudo
+      
+      Eu não quero te deixar
+      Sem seu toque, eu não vou mais ficar
+      porque te quero além de tudo
+      Eu te quero além de tudo
+
+      Toda terra  e mar
+      mais que o respirar
+      Eu te quero além de tudo
+      Eu te quero mais que tudo
+
+      Eu não quero te soltar
+      Eu não quero te soltar
+      Eu não quero te soltar
+      Eu te quero além de tudo
+      `
+  },
 ];
 
 export const getCategoryBySlug = (slug: string) => { 
